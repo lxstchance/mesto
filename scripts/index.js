90c610edcaafd69f7956897b5080d4b2adafd443
Stas Kirpichnikov <kirpichnikovstas@gmail.com>
2022-12-17 14:15:10 +0300

    fix: исправлена функция изменения имени и подправлен адаптив

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,38 @@
-const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.popup__close-btn');
-const editForm = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close-btn');
 
-function openForm() {
-    editForm.classList.remove('popup_disabled')
+function openPopup() {
+    popup.classList.add('popup_opened');
 }
 
-function closeForm() {
-    editForm.classList.add('popup_disabled');
+editButton.addEventListener('click', openPopup);
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
 }
 
-editButton.addEventListener('click', openForm);
-closeButton.addEventListener('click', closeForm);
+closeButton.addEventListener('click', closePopup);
 
-editForm.addEventListener('click', (event) => {
+popup.addEventListener('click', (event) => {
     if (event.target === event.currentTarget) {
-        closeForm();
+        popup.classList.remove('popup_opened');
     }
 })
 
-const submitButton = editForm.querySelector('.popup__submit-btn');
-let formName = editForm.querySelector('.popup__name');
-let formCareer = editForm.querySelector('.popup__career');
+let popupElement = document.querySelector('.popup__container');
+let nameInput = popupElement.querySelector('.popup__name');
+let careerInput = popupElement.querySelector('.popup__career');
 let profileName = document.querySelector('.profile__title');
 let profileCareer = document.querySelector('.profile__subtitle');
+let popupSaveButton = popupElement.querySelector('.popup__submit-btn');
 
-function changeName() {
-    profileName.textContent = formName.value;
-    profileCareer.textContent = formCareer.value;
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileCareer.textContent = careerInput.value;
 }
 
-submitButton.addEventListener('click', changeName);
-submitButton.addEventListener('click', closeForm);
+popupElement.addEventListener('submit', handleFormSubmit);
+popupSaveButton.addEventListener('click', closePopup);
