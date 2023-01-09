8bb395a4cbea006394d53e929fd330e1eda8cdc3
Stas Kirpichnikov <kirpichnikovstas@gmail.com>
2023-01-09 07:35:39 +0300

    feat: like button

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,54 @@
-let editButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = popup.querySelector('.popup__close-btn');
+// TODO // 1. Шесть карточек «из коробки»
+// При загрузке на странице должно быть 6 карточек, которые добавит JavaScript.
+// Их названия и фотографии выберите сами или возьмите готовый массив
+// TODO 2. Форма добавления карточки
+// Добавьте в проект форму добавления новой карточки.Макет найдёте в «Фигме».
+// Сделайте так, чтобы форма открывалась нажатием на кнопку «+» и закрывалась кликом на крестик
+// TODO 3.Добавление карточки
+// Дайте пользователю возможность добавлять карточки
+// TODO 5. Удаление карточки
+// Добавьте карточкам иконку удаления.Должно выглядеть как в макете
+// TODO 6. Открытие попапа с картинкой
+// Настройте просмотр фотографий.Пусть открываются нажатием на картинку и закрываются кликом на крестик
+// TODO 7. Плавное открытие и закрытие попапов
+// Сделайте так, чтобы все попапы плавно открывались.Пусть проявляются из прозрачности и уходят в неё при закрытии
+
+const profileButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupClose = popup.querySelector('.popup__close-btn');
+const popupElement = document.querySelector('.popup__inner');
+const nameInput = popupElement.querySelector('#input-name');
+const careerInput = popupElement.querySelector('#input-career');
+const profileName = document.querySelector('.profile__title');
+const profileCareer = document.querySelector('.profile__subtitle');
+const elementLike = document.querySelectorAll('.element__heart-button');
+
+//Add like in element
+elementLike.forEach((item) => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('element__heart-button_active');
+    })
+});
+
 
 function openPopup() {
     popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    careerInput.value = profileCareer.textContent;
 }
-
-editButton.addEventListener('click', openPopup);
 
 function closePopup() {
     popup.classList.remove('popup_opened');
 }
 
-closeButton.addEventListener('click', closePopup);
-
-popup.addEventListener('click', (event) => {
-    if (event.target === event.currentTarget) {
-        popup.classList.remove('popup_opened');
-    }
-})
-
-let popupElement = document.querySelector('.popup__container');
-let nameInput = popupElement.querySelector('.popup__name');
-let careerInput = popupElement.querySelector('.popup__career');
-let profileName = document.querySelector('.profile__title');
-let profileCareer = document.querySelector('.profile__subtitle');
-let popupSaveButton = popupElement.querySelector('.popup__submit-btn');
-
-
 function handleFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileCareer.textContent = careerInput.value;
+    closePopup();
 }
 
+
+profileButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
 popupElement.addEventListener('submit', handleFormSubmit);
-popupSaveButton.addEventListener('click', closePopup);
