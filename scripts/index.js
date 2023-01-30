70d226396c65e289bd3cc2321bdaf0ab53620d16
Stas Kirpichnikov <kirpichnikovstas@gmail.com>
2023-01-30 06:58:22 +0300

    feat: close Esc

--- scripts/index.js
+++ scripts/index.js
@@ -28,15 +28,25 @@ const formSrc = popupElements.querySelector('.popup__link');
 const bigImage = popupImage.querySelector('.popup__image');
 const bigText = popupImage.querySelector('.popup__text');
 
-
 // Открытие попапа
 function openPopup(element) {
     element.classList.add('popup_opened');
+    document.addEventListener('keydown', closePopupESC);
 }
 
 //Закрытие попапа
 function closePopup(element) {
     element.classList.remove('popup_opened');
+    document.removeEventListener('keydown', closePopupESC);
+}
+
+
+//Закрытие через Esc 
+function closePopupESC(evt) {
+    if (evt.key === `Escape`) {
+        const popup = document.querySelector('.popup_opened');
+        closePopup(popup);
+    }
 }
 
 // Заполнение профиля
